2952baed5f091ab42c59b55f0f1ba9e4450cd5a1
Koenraad <koenemans@proton.me>
2025-12-15 11:19:01 +0100

    Update imports in logger.test.js

--- server/src/__tests__/logger.test.js
+++ server/src/__tests__/logger.test.js
@@ -1,4 +1,11 @@
-import { beforeEach, describe, expect, jest, test } from '@jest/globals';
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  jest,
+  test,
+} from '@jest/globals';
 import { createRequestLogger, Logger } from '../utils/logger.js';
 
 describe('Logger', () => {
